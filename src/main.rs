6962d6bc3455ad2495ad0d58ab04f5a5a1d59abe
Alexandru Vasile <alexandru.vasile@parity.io>
2024-08-29 13:45:10 +0300

    Enhance decoding of chainHead::events
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- src/main.rs
+++ src/main.rs
@@ -60,6 +60,77 @@ pub enum StorageQueryType {
     DescendantsHashes,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Init {
+    event: String,
+    finalizedBlockHashes: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Finalized {
+    event: String,
+    finalizedBlockHashes: Vec<String>,
+    prunedBlockHashes: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct NewBlock {
+    event: String,
+    blockHash: String,
+    parentBlockHash: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct BestBlock {
+    event: String,
+    bestBlockHash: String,
+}
+
+enum Event {
+    Init(Init),
+    Finalized(Finalized),
+    NewBlock(NewBlock),
+    BestBlock(BestBlock),
+}
+
+fn parse_event(event: Box<RawValue>) -> Option<Event> {
+    if let Ok(result) = serde_json::from_str::<Init>(&event.to_string()) {
+        if result.event == "initialized" {
+            return Some(Event::Init(result));
+        } else {
+            if result.event != "finalized" {
+                panic!("Expected event to be finalized");
+            }
+
+            return Some(Event::Finalized(Finalized {
+                event: result.event,
+                finalizedBlockHashes: result.finalizedBlockHashes,
+                prunedBlockHashes: vec![],
+            }));
+        }
+    }
+
+    if let Ok(result) = serde_json::from_str::<NewBlock>(&event.to_string()) {
+        if result.event != "newBlock" {
+            panic!("Expected event to be new");
+        }
+
+        return Some(Event::NewBlock(result));
+    }
+
+    if let Ok(result) = serde_json::from_str::<BestBlock>(&event.to_string()) {
+        if result.event != "bestBlockChanged" {
+            panic!("Expected event to be best");
+        }
+
+        return Some(Event::BestBlock(result));
+    }
+
+    tracing::info!("Ignoring chainHead event: {:?}\n", event);
+
+    None
+}
+
 async fn subscribe() -> Result<(), Box<dyn std::error::Error>> {
     // let client = client("ws://localhost:9944").await?;
     let client = client("wss://rpc.polkadot.io:443").await?;
@@ -72,7 +143,7 @@ async fn subscribe() -> Result<(), Box<dyn std::error::Error>> {
     )
     .await?;
 
-    println!("Subscription ID: {:?}\n", subscription.kind());
+    tracing::info!("Subscription ID: {:?}", subscription.kind());
 
     let sub_id = match subscription.kind().clone() {
         jsonrpsee::core::client::SubscriptionKind::Subscription(sub) => match sub {
@@ -82,9 +153,7 @@ async fn subscribe() -> Result<(), Box<dyn std::error::Error>> {
         jsonrpsee::core::client::SubscriptionKind::Method(_) => todo!(),
         _ => todo!(),
     };
-    println!("Sub id {:?}", sub_id);
-
-    tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+    tracing::info!("Sub id string {:?}\n", sub_id);
 
     #[derive(Debug, PartialEq, Eq)]
     enum BlockState {
@@ -94,124 +163,113 @@ async fn subscribe() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     let mut blocks = HashMap::new();
+    let mut recv_init = false;
 
     while let Some(event) = subscription.next().await {
         let event = event?;
+        tracing::info!("Raw event: {:?}", event);
 
-        #[derive(Serialize, Deserialize, Debug)]
-        struct Init {
-            event: String,
-            finalizedBlockHashes: Vec<String>,
-        }
+        let Some(event) = parse_event(event) else {
+            continue;
+        };
 
-        #[derive(Serialize, Deserialize, Debug)]
-        struct Finalized {
-            event: String,
-            finalizedBlockHashes: Vec<String>,
-            prunedBlockHashes: Vec<String>,
-        }
+        match event {
+            Event::Init(result) => {
+                if recv_init {
+                    panic!("Received multiple init events");
+                }
+                recv_init = true;
 
-        #[derive(Serialize, Deserialize, Debug)]
-        struct NewBlock {
-            event: String,
-            blockHash: String,
-            parentBlockHash: String,
-        }
+                tracing::info!("Init event: {:?}", result);
 
-        #[derive(Serialize, Deserialize, Debug)]
-        struct BestBlock {
-            event: String,
-            bestBlockHash: String,
-        }
+                for hash in result.finalizedBlockHashes {
+                    tracing::info!("  Finalized block hash: {:?}", hash);
 
-        if let Ok(result) = serde_json::from_str::<Init>(&event.to_string()) {
-            println!("Init event: {:?}\n", result);
+                    let response: Box<RawValue> = client
+                        .request("chainHead_v1_unpin", rpc_params![sub_id.clone(), hash])
+                        .await?;
+                    tracing::info!("  Response for unpinning {}", response);
+                }
 
-            for hash in result.finalizedBlockHashes {
-                println!("  Finalized block hash: {:?}\n", hash);
-
-                let response: Box<RawValue> = client
-                    .request("chainHead_v1_unpin", rpc_params![sub_id.clone(), hash])
-                    .await?;
-                println!(" Response for unpinning {}", response);
+                tracing::info!("\n");
             }
 
-            continue;
-        }
+            Event::NewBlock(result) => {
+                tracing::info!("NewBlock event: {:?}", result);
 
-        if let Ok(result) = serde_json::from_str::<NewBlock>(&event.to_string()) {
-            println!("NewBlock event: {:?}\n", result);
+                tracing::info!(" block entry {:?}", blocks.entry(result.blockHash.clone()));
 
-            match blocks.entry(result.blockHash.clone()) {
-                std::collections::hash_map::Entry::Occupied(entry) => {
-                    panic!(
-                        "
+                match blocks.entry(result.blockHash.clone()) {
+                    std::collections::hash_map::Entry::Occupied(entry) => {
+                        panic!(
+                            "
                         Block hash already exists in the map.
                         Current state: {:?}
                         New state: {:?}
                         Hash {:?}
                         ",
-                        entry.get(),
-                        BlockState::New,
-                        result.blockHash
-                    );
+                            entry.get(),
+                            BlockState::New,
+                            result.blockHash
+                        );
+                    }
+                    std::collections::hash_map::Entry::Vacant(entry) => {
+                        tracing::info!("  - This is a new entry");
+                        entry.insert(BlockState::New);
+                    }
                 }
-                std::collections::hash_map::Entry::Vacant(entry) => {
-                    println!("  - This is a new entry");
-                    entry.insert(BlockState::New);
-                }
+
+                blocks.insert(result.blockHash.clone(), BlockState::New);
+                tracing::info!("\n");
             }
 
-            blocks.insert(result.blockHash.clone(), BlockState::New);
+            Event::BestBlock(result) => {
+                tracing::info!("BestBlock event: {:?}", result);
+                tracing::info!(
+                    " block entry {:?}",
+                    blocks.entry(result.bestBlockHash.clone())
+                );
 
-            continue;
-        }
-
-        if let Ok(result) = serde_json::from_str::<BestBlock>(&event.to_string()) {
-            println!("BestBlock event: {:?}\n", result);
-
-            match blocks.entry(result.bestBlockHash.clone()) {
-                std::collections::hash_map::Entry::Occupied(mut entry) => {
-                    println!("  - Advancing block to Best state from {:?}", entry.get());
-                    if entry.get() != &BlockState::New {
-                        panic!(
-                            "
+                match blocks.entry(result.bestBlockHash.clone()) {
+                    std::collections::hash_map::Entry::Occupied(mut entry) => {
+                        tracing::info!("  - Advancing block to Best state from {:?}", entry.get());
+                        if entry.get() != &BlockState::New {
+                            panic!(
+                                "
                             (best occupied) Expected block entry occupied with New state
                             Current state: {:?}
                             Hash {:?}
                             ",
-                            entry.get(),
-                            result.bestBlockHash
-                        );
-                    }
+                                entry.get(),
+                                result.bestBlockHash
+                            );
+                        }
 
-                    *entry.get_mut() = BlockState::Best;
-                }
-                std::collections::hash_map::Entry::Vacant(_entry) => {
-                    panic!(
-                        "
+                        *entry.get_mut() = BlockState::Best;
+                    }
+                    std::collections::hash_map::Entry::Vacant(_entry) => {
+                        panic!(
+                            "
                         (best vacant) Expected block entry occupied with New state
                         "
-                    );
+                        );
+                    }
                 }
             }
 
-            continue;
-        }
+            Event::Finalized(result) => {
+                tracing::info!("Finalized event: {:?}", result);
 
-        if let Ok(result) = serde_json::from_str::<Finalized>(&event.to_string()) {
-            println!("Finalized event: {:?}\n", result);
-
-            for hash in result
-                .finalizedBlockHashes
-                .iter()
-                .chain(result.prunedBlockHashes.iter())
-            {
-                match blocks.entry(hash.clone()) {
-                    std::collections::hash_map::Entry::Occupied(mut entry) => {
-                        println!(" - Finalizing block from {:?}", entry.get());
-                        if entry.get() != &BlockState::New || entry.get() != &BlockState::Best {
-                            panic!(
+                for hash in result
+                    .finalizedBlockHashes
+                    .iter()
+                    .chain(result.prunedBlockHashes.iter())
+                {
+                    match blocks.entry(hash.clone()) {
+                        std::collections::hash_map::Entry::Occupied(mut entry) => {
+                            tracing::info!(" - Finalizing block from {:?}", entry.get());
+                            if entry.get() != &BlockState::New && entry.get() != &BlockState::Best {
+                                panic!(
                                 "
                                 (finalized occupied) Expected block entry occupied with New or Best state
                                 Current state: {:?}
@@ -220,32 +278,31 @@ async fn subscribe() -> Result<(), Box<dyn std::error::Error>> {
                                 entry.get(),
                                 hash
                             );
-                        }
+                            }
 
-                        *entry.get_mut() = BlockState::Finalized;
-                    }
-                    std::collections::hash_map::Entry::Vacant(_entry) => {
-                        panic!(
-                            "
+                            *entry.get_mut() = BlockState::Finalized;
+                        }
+                        std::collections::hash_map::Entry::Vacant(_entry) => {
+                            panic!(
+                                "
                             (finalized vacant) Expected block entry for {:?}
                             ",
-                            hash,
-                        );
+                                hash,
+                            );
+                        }
                     }
+
+                    tracing::info!("  Unpining block hash: {:?}\n", hash);
+
+                    let response: Box<RawValue> = client
+                        .request("chainHead_v1_unpin", rpc_params![sub_id.clone(), hash])
+                        .await?;
+                    tracing::info!(" Response for unpinning {}", response);
                 }
 
-                println!("  Unpining block hash: {:?}\n", hash);
-
-                let response: Box<RawValue> = client
-                    .request("chainHead_v1_unpin", rpc_params![sub_id.clone(), hash])
-                    .await?;
-                println!(" Response for unpinning {}", response);
+                tracing::info!("\n");
             }
-
-            continue;
         }
-
-        println!("ChainHead event: {:?}\n", event);
     }
 
     Ok(())
